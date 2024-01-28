7d78fdfbbee14d52c5808bc22cdce151b894c6e4
estudart <erico.studartdf@gmail.com>
2024-01-28 16:33:49 -0300

    changing the user schema

--- schemas/user.py
+++ schemas/user.py
@@ -1,8 +1,10 @@
-from marshmallow import Schema
+from marshmallow import Schema, fields
 
 class UserSchema(Schema):
-    class Meta:
-        fields = ('id', 'first_name', 'email', 'password')
+    id = fields.Int()
+    first_name = fields.Str()
+    email = fields.Str()
+    password = fields.Str()
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
